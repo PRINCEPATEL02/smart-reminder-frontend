7d6b3dfa046dd1a4cf767fc234ad457d98182a07
Prince (PRINCEPATEL02) <princespatel02@gmail.com>
2026-02-13 08:54:47 +0530

    Initial backend commit

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.6dmhrh3loa8"
+    "revision": "0.991lljtl7j"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
